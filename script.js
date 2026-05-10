5485da56a8a764299bf28b8061bada90ae789602
Lyss2120 <lys.ivonne@gmail.com>
2026-05-10 13:34:41 -0400

    contact-info, footer

--- script.js
+++ script.js
@@ -2,10 +2,43 @@ const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 // const navMenu = document.querySelector(".nav-menu");
 
-menuOpenButton.addEventListener('click', () =>{
+menuOpenButton.addEventListener('click', () => {
     document.body.classList.toggle('show-mobile-menu');
 })
 
 menuCloseButton.addEventListener('click', () => {
     menuOpenButton.click()
 })
+
+// slider icianization
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    // spaceBetween: 25,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // responsive behavior
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
